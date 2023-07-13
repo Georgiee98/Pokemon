723ccf50b4801b9e264fec4985e676ca8a0a5bf6
“Gjorgi <georgiedelev@gmail.com>
2023-07-13 19:25:00 +0200

    Details Dynamical Stats Done

--- script.js
+++ script.js
@@ -58,55 +58,121 @@ fetch("source.json")
 
     main.appendChild(details);
 
+
+
+
     let pokemonHPStats = document.createElement('div');
     pokemonHPStats.setAttribute('id', 'stats')
     details.append(pokemonHPStats)
+
     let pokemonHP = document.createElement("p");
     pokemonHP.setAttribute("id", "hp");
     pokemonHPStats.append(pokemonHP);
+
     let pokemonHP_div = document.createElement("div");
     pokemonHP_div.setAttribute("id", "hp_div");
     pokemonHPStats.append(pokemonHP_div);
+
     let pokemonHPStatsFullWidth = document.createElement('p')
     pokemonHPStatsFullWidth.setAttribute('id', 'pokemonStatsFullWidth')
     pokemonHP_div.append(pokemonHPStatsFullWidth)
 
 
 
-    let pokemonATK_div = document.createElement("div");
-    pokemonATK_div.setAttribute("id", "hp_div");
-    details.append(pokemonATK_div);
+
+    let pokemonATKStats = document.createElement('div');
+    pokemonATKStats.setAttribute('id', 'stats')
+    details.append(pokemonATKStats)
+
     let pokemonATK = document.createElement("p");
     pokemonATK.setAttribute("id", "hp");
-    pokemonATK_div.appendChild(pokemonATK);
+    pokemonATKStats.appendChild(pokemonATK);
+
+    let pokemonATK_div = document.createElement("div");
+    pokemonATK_div.setAttribute("id", "hp_div");
+    pokemonATKStats.append(pokemonATK_div);
+
+    let pokemonATKStatsFullWidth = document.createElement('p')
+    pokemonATKStatsFullWidth.setAttribute('id', 'pokemonStatsFullWidth')
+    pokemonATK_div.append(pokemonATKStatsFullWidth)
+
+
+
+
+    let pokemonDEFStats = document.createElement('div');
+    pokemonDEFStats.setAttribute('id', 'stats')
+    details.append(pokemonDEFStats)
+
+    let pokemonDEF = document.createElement("p");
+    pokemonDEF.setAttribute("id", "hp");
+    pokemonDEFStats.appendChild(pokemonDEF);
 
     let pokemonDEF_div = document.createElement("div");
     pokemonDEF_div.setAttribute("id", "hp_div");
-    details.append(pokemonDEF_div);
-    let pokemonDEF = document.createElement("p");
-    pokemonDEF.setAttribute("id", "hp");
-    pokemonDEF_div.appendChild(pokemonDEF);
+    pokemonDEFStats.append(pokemonDEF_div);
+
+    let pokemonDEFStatsFullWidth = document.createElement('p')
+    pokemonDEFStatsFullWidth.setAttribute('id', 'pokemonStatsFullWidth')
+    pokemonDEF_div.append(pokemonDEFStatsFullWidth)
+
+
+
+    let pokemonSPatkStats = document.createElement('div');
+    pokemonSPatkStats.setAttribute('id', 'stats')
+    details.append(pokemonSPatkStats)
+
+    let pokemonSPatk = document.createElement("p");
+    pokemonSPatk.setAttribute("id", "hp");
+    pokemonSPatkStats.appendChild(pokemonSPatk);
 
     let pokemonSPatk_div = document.createElement("div");
     pokemonSPatk_div.setAttribute("id", "hp_div");
-    details.append(pokemonSPatk_div);
-    let pokemonSPatk = document.createElement("p");
-    pokemonSPatk.setAttribute("id", "hp");
-    pokemonSPatk_div.appendChild(pokemonSPatk);
+    pokemonSPatkStats.append(pokemonSPatk_div);
+
+    let pokemonSPatkStatsFullWidth = document.createElement('p')
+    pokemonSPatkStatsFullWidth.setAttribute('id', 'pokemonStatsFullWidth')
+    pokemonSPatk_div.append(pokemonSPatkStatsFullWidth)
+
+
+
+
+
+
+    let pokemonSPdefStats = document.createElement('div');
+    pokemonSPdefStats.setAttribute('id', 'stats')
+    details.append(pokemonSPatkStats)
+
+    let pokemonSPdef = document.createElement("p");
+    pokemonSPdef.setAttribute("id", "hp");
+    pokemonSPdefStats.appendChild(pokemonSPdef);
 
     let pokemonSPdef_div = document.createElement("div");
     pokemonSPdef_div.setAttribute("id", "hp_div");
-    details.append(pokemonSPdef_div);
-    let pokemonSPdef = document.createElement("p");
-    pokemonSPdef.setAttribute("id", "hp");
-    pokemonSPdef_div.appendChild(pokemonSPdef);
+    pokemonSPdefStats.append(pokemonSPdef_div);
+
+    let pokemonSPdefStatsFullWidth = document.createElement('p')
+    pokemonSPdefStatsFullWidth.setAttribute('id', 'pokemonStatsFullWidth')
+    pokemonSPdef_div.append(pokemonSPdefStatsFullWidth)
+
+
+
+
+    let pokemonSpeedStats = document.createElement('div');
+    pokemonSpeedStats.setAttribute('id', 'stats')
+    details.append(pokemonSpeedStats)
+
+
+    let pokemonSpeed = document.createElement("p");
+    pokemonSpeed.setAttribute("id", "hp");
+    pokemonSpeedStats.appendChild(pokemonSpeed);
 
     let pokemonSpeed_div = document.createElement("div");
     pokemonSpeed_div.setAttribute("id", "hp_div");
-    let pokemonSpeed = document.createElement("p");
-    details.append(pokemonSpeed_div);
-    pokemonSpeed.setAttribute("id", "hp");
-    pokemonSpeed_div.appendChild(pokemonSpeed);
+    pokemonSpeedStats.append(pokemonSpeed_div);
+
+    let pokemonSpeedStatsFullWidth = document.createElement('p')
+    pokemonSpeedStatsFullWidth.setAttribute('id', 'pokemonStatsFullWidth')
+    pokemonSpeed_div.append(pokemonSpeedStatsFullWidth)
 
 
     // console.log(pokemon.name['english'])
@@ -142,19 +208,24 @@ fetch("source.json")
     pokemonHPStatsFullWidth.style.width = pokemon.base['HP'] > 119 ? "119%" : pokemon.base['HP'] + "%"
 
     pokemonATK.textContent = 'Attack: ' + pokemon.base['Attack']
-    pokemonATK.style.width = pokemon.base['Attack'] > 119 ? "119%" :pokemon.base['Attack'] + "%"
+    // pokemonATK.style.width = pokemon.base['Attack'] > 119 ? "119%" :pokemon.base['Attack'] + "%"
+    pokemonATKStatsFullWidth.style.width = pokemon.base['HP'] > 119 ? "119%" : pokemon.base['HP'] + "%"
 
     pokemonDEF.textContent = 'Defense: ' + pokemon.base['Defense']
-    pokemonDEF.style.width = pokemon.base['Defense'] > 119 ? "119%" : pokemon.base['Defense'] + "%"
+    // pokemonDEF.style.width = pokemon.base['Defense'] > 119 ? "119%" : pokemon.base['Defense'] + "%"
+    pokemonDEFStatsFullWidth.style.width = pokemon.base['HP'] > 119 ? "119%" : pokemon.base['HP'] + "%"
 
     pokemonSPatk.textContent = 'Sp.Atk: ' + pokemon.base['Sp. Attack']
-    pokemonSPatk.style.width = pokemon.base['Sp. Attack'] > 119 ? "119" : pokemon.base['Sp. Attack'] + "%"
+    // pokemonSPatk.style.width = pokemon.base['Sp. Attack'] > 119 ? "119" : pokemon.base['Sp. Attack'] + "%"
+    pokemonSPatkStatsFullWidth.style.width = pokemon.base['HP'] > 119 ? "119%" : pokemon.base['HP'] + "%"
 
     pokemonSPdef.textContent = 'Sp.Def: ' + pokemon.base['Sp. Defense']
-    pokemonSPdef.style.width = pokemon.base['Sp. Defense'] > 119 ? "119%" : pokemon.base['Sp. Defense'] + "%"
+    // pokemonSPdef.style.width = pokemon.base['Sp. Defense'] > 119 ? "119%" : pokemon.base['Sp. Defense'] + "%"
+    pokemonSPdefStatsFullWidth.style.width = pokemon.base['HP'] > 119 ? "119%" : pokemon.base['HP'] + "%"
 
     pokemonSpeed.textContent = 'Speed: ' + pokemon.base['Speed']
-    pokemonSpeed.style.width = pokemon.base['Speed'] > 119 ? "119%" : pokemon.base['Speed'] + "%"
+    // pokemonSpeed.style.width = pokemon.base['Speed'] > 119 ? "119%" : pokemon.base['Speed'] + "%"
+    pokemonSpeedStatsFullWidth.style.width = pokemon.base['HP'] > 119 ? "119%" : pokemon.base['HP'] + "%"
 
 
 
